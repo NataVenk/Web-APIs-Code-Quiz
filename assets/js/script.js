a6b767bb9f0334a9c48e0cb6319aae4d8edc6b0c
Natalia Venkatesh <nataliavenkatesh@macbook-pro.lan>
2022-09-28 12:10:33 -0400

    initial files set up and some of code outlined

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+console.log("here i am")
+
+var timerEl=document.querySelector(".timer");
+
+var question = document.getElementById(".question")
+
+var secondsLLeft = 60;
+
+// need a collection of objects that hold quesitons
+
+
+// need click start button 
+
+startQuiz.addEventListener("click", function() {
+    
+    }
+
+
+function.startTimer(
+
+    var timerInterval=setInterval(function()){
+
+        secondsLeft--;  
+
+        if (secondsLeft==0){
+            clearInterval(timerInterval);
+            // go to final score window with inititals and submit button
+        }
+    }
+    
+)
+
+// quizz starts
+function.startQuiz(){
+    
+
+}
+
+// Function call to present questions
+
+function SendMessage(){
+    var questionEl = document.createElement ('question')
+    questionEl.setAttribute 
+
+
+}

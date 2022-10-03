d4e5a32bf76493585dfc219489892360fd796f49
Natalia Venkatesh <nataliavenkatesh@MacBook-Pro.local>
2022-10-03 17:28:26 -0400

    added timer function, need to show it on the screen as a button, added funtionof removing front page when quiz starts, need to add end page and score count

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,31 @@
 console.log("here i am")
 
 var timerEl=document.querySelector(".timer");
-
 var timerCount, userScore, questionindex;
+
+
+var timerCount = 60;
+
 var listAnswers = document.querySelector(".list-answers");
 var startButton = document.querySelector(".start-button");
 var resetButton = document.querySelector(".reset-button");
-var question = document.getElementById("question")
 
+var question = document.getElementById("question")
 
 var choice1 = document.getElementById("choice1")
 var choice2 = document.getElementById("choice2")
 var choice3 = document.getElementById("choice3")
 var choice4 = document.getElementById("choice4")
+
+var startQ = document.querySelector("#front-page");
+var endQ = document.querySelector("#end-page");
+// var h1 = document.querySelector ('h1')
+// var title=document.getElementById("titleQ");
+// title.setAttribute("style", '100px', 'center');
+// h1.append(title);
+
+// resetButton.setAttribute;
+
 
 var questionArr=[{
     question:"this is question #1",
@@ -35,41 +48,77 @@ var questionArr=[{
     choiceC: "3this is wrong",
     choiceD: "3this is correct",
     correct: "3this is correct"
-}]
+},{
+    question:"this is question #4",
+    choiceA: "4this is correct",
+    choiceB: "4this is wrong",
+    choiceC: "4this is wrong",
+    choiceD: "4this is wrong",
+    correct: "4this is correct"
+}
+]
 
 
-// need a collection of objects that hold quesitons
 
+// function setTime() {
+//   // Sets interval in variable
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft;
 
-// need click start button 
+//     if(secondsLeft === 0) {
+      
+//       clearInterval(timerInterval);
+      
+//     //   sendMessage();
+//     }
 
-// function.startQuiz(){
+//   }, 1000);
 // }
+
+// // Function to create and append colorsplosion image
+// function sendMessage() {
+//   timeEl.textContent = " ";
+//   var imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+
+// }
+
+// setTime();
+
+
+
+// start quiz and switch from front page to questions
 
 function startQuiz() {
 
     timerCount = 60;
     userScore = 0;
-    questionindex=0;
+    questionindex = 0;
     renderQuestion()
     startTimer()
     startButton.disabled=true;
-  }
+    startQ.style.display = "block";
+    for(var i = 0; i < questionArr.length; i++){
+        startQ.style.display = "none";
+    }
+    
+}
+   
+
 
 function startTimer(){
 
-    // timerInterval=setInterval(function()){
-    //     timerCount--;  
-    //     timerElement.textContent = timerCount;
-
-    //     if (timerCount==0){
-    //         clearInterval(timer);
-    //         resultsEl ();
-    //         // go to final score window with inititals and submit button
-    //     }
-    // }, 1000
-}    
+    var timerCount=setInterval(function(){
+        timerCount--; 
+        if (timerCount===0){
+            stop(timer)};
+    },1000);
+ 
     
+}
+
 
 
 startButton.addEventListener("click", startQuiz);
@@ -84,8 +133,6 @@ function renderQuestion(){
    choice2.textContent=questionArr[questionindex].choiceB
    choice3.textContent=questionArr[questionindex].choiceC
    choice4.textContent=questionArr[questionindex].choiceD
-//    question.textContent=questionArr[1].question
-//    question.textContent=questionArr[2].question
 
 }
 function checkAnswer(event){
@@ -95,9 +142,6 @@ function checkAnswer(event){
 
 }
 
-document.querySelector(".list-answers").addEventListener("click", checkAnswer);
-
-startButton.addEventListener("click", startQuiz);
-
+document.querySelector(".list-answers").addEventListener("click", checkAnswer)
 
 
